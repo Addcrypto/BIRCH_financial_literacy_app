143624d009e362cf0faf611e4bed7282478fb732
Hydani <dhan4733@gmail.com>
2022-09-25 00:22:41 -0500

    Created IntroScreen, Login/Register, and Connected to Firebase

---
+++ app/src/main/java/com/example/birch/IntroScreen.java
@@ -0,0 +1,32 @@
+package com.example.birch;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class IntroScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_screen);
+
+        IntroThread introThread = new IntroThread(handler);
+        introThread.start();
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what == 1){
+                Intent intent = new Intent(IntroScreen.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
+
+}

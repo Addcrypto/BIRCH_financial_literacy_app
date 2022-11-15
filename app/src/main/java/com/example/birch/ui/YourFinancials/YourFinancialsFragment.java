0ec961a9e41716a6d9d35735f4cf66ffaedc1fec
Miguel <miguellealw@gmail.com>
2022-11-15 10:27:46 -0600

    added event listeners to link buttons

--- app/src/main/java/com/example/birch/ui/YourFinancials/YourFinancialsFragment.java
+++ app/src/main/java/com/example/birch/ui/YourFinancials/YourFinancialsFragment.java
@@ -12,8 +12,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.birch.R;
+import com.example.birch.RegisterActivity;
 import com.example.birch.models.BankInfoModel;
 import com.example.birch.ui.Home.BankInfo_RecyclerViewAdapter;
 
@@ -38,6 +40,8 @@ import java.util.Arrays;
 public class YourFinancialsFragment extends Fragment {
     ArrayList<BankInfoModel> bankInfoModels = new ArrayList<>();
     ArrayList<BankInfoModel> debtInfoModels = new ArrayList<>();
+    Button btn_linkAccount;
+    Button btn_linkDebt;
 
     //Retrieval of Link Token
     LinkTokenConfiguration linkTokenConfiguration = new LinkTokenConfiguration.Builder()
@@ -71,50 +75,13 @@ public class YourFinancialsFragment extends Fragment {
             }
     );
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public YourFinancialsFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment your_financials.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static YourFinancialsFragment newInstance(String param1, String param2) {
-        YourFinancialsFragment fragment = new YourFinancialsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        /*
-        TO DO:
-        OnClickLister for each link button
-         */
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
         // always set up models before passing to recycler view
         setUpBankInfoModels();
@@ -145,6 +112,23 @@ public class YourFinancialsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_your_financials, container, false);
         Context ctx = getActivity().getApplicationContext();
+
+        btn_linkAccount = view.findViewById(R.id.btn_linkBank);
+        btn_linkDebt = view.findViewById(R.id.btn_linkDebt);
+
+        btn_linkAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ctx, "link account clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_linkDebt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ctx, "link debt clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         RecyclerView rv_yourFinancials = view.findViewById(R.id.rv_home_YourFinancials);
         BankInfo_RecyclerViewAdapter adapter = new BankInfo_RecyclerViewAdapter(ctx, bankInfoModels);

ceb0bd2ed587c453f6a65f4a84e7d1d102e4a522
PabloSan19 <pablo.sanchez13@outlook.com>
2022-11-15 14:10:53 -0600

    Added PlaidTest activity to test the api and its calls and network folder to talk to the server.

--- app/src/main/java/com/example/birch/ui/Settings/SettingsFragment.java
+++ app/src/main/java/com/example/birch/ui/Settings/SettingsFragment.java
@@ -1,13 +1,26 @@
 package com.example.birch.ui.Settings;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
 
 import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.Toast;
 
+import com.example.birch.MainActivity;
+import com.example.birch.PlaidTest;
 import com.example.birch.R;
 
 /**
@@ -21,6 +34,7 @@ public class SettingsFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    Button bt_plaids;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -55,12 +69,31 @@ public class SettingsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        //bt_plaid = (Button) getView().findViewById(R.id.bt_plaid);
+//        bt_plaid.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getActivity(), PlaidTest.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        bt_plaids = view.findViewById(R.id.bt_plaid);
+        bt_plaids.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), PlaidTest.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
